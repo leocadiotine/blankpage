b7f86871386272a47290760901e65ebc29223024
Leocadio Tiné <leocadiotine@gmail.com>
2013-12-24 04:31:37 -0300

    Persists the text when the activity is paused.

--- code/src/io/leocad/blankpage/BlankPageActivity.java
+++ code/src/io/leocad/blankpage/BlankPageActivity.java
@@ -2,12 +2,15 @@ package io.leocad.blankpage;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
 public class BlankPageActivity extends Activity {
+	
+	private static final String KEY_TEXT = "oehuboetudoteuioetuxotuxkt";
 	
 	private EditText mEditText;
 
@@ -17,7 +20,28 @@ public class BlankPageActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_blank_page);
 		
-		mEditText = (EditText) findViewById(R.id.et);
+		mEditText = (EditText) findViewById(R.id.et);		
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		final String text = getPreferences(MODE_PRIVATE).getString(KEY_TEXT, null);
+		mEditText.setText(text);
+	}
+	
+	@Override
+	protected void onPause() {
+	
+		final String text = mEditText.getText().toString();
+		
+		getPreferences(MODE_PRIVATE)
+		.edit()
+		.putString(KEY_TEXT, text)
+		.commit();
+		
+		super.onPause();
 	}
 
 	@Override
